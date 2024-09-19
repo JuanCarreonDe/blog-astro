28ee8c41000d14b3b5fccee0aac574443740e439
juancarreonde <juancarreon@gmail.com>
2024-09-19 15:57:44 -0600

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: "https://juan-blog.netlify.app",
+  site: "https://main--juan-blog-astro.netlify.app/",
 });
