cf02d5b7956d6b7788651d0fd3d41cffc1afec2b
juancarreonde <juancarreon@gmail.com>
2024-09-19 15:53:51 -0600

    first commit

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -1,4 +1,4 @@
 export const siteConfig = {
-  title: "Fernando's Blog",
-  description: 'Blog oficial de Fernando Herrera - Web developer',
+  title: "juan's Blog",
+  description: "Blog oficial de juan Herrera - Web developer",
 };
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,29 +1,29 @@
-import rss from '@astrojs/rss';
-import type { APIRoute } from 'astro';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
 
-import MarkdownIt from 'markdown-it';
-import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
 
 const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
-  const blogPosts = await getCollection('blog');
+  const blogPosts = await getCollection("blog");
 
   return rss({
     // stylesheet: '/styles/rss.xsl',
     // `<title>` field in output xml
-    title: 'Fernando Blog',
+    title: "juan Blog",
     // `<description>` field in output xml
-    description: 'Un simple blog sobre mis aventura con Astro',
+    description: "Un simple blog sobre mis aventura con Astro",
 
     xmlns: {
-      media: 'http://search.yahoo.com/mrss/',
+      media: "http://search.yahoo.com/mrss/",
     },
 
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#contextsite
-    site: site ?? '',
+    site: site ?? "",
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
     items: blogPosts.map(({ data, slug, body }) => ({
@@ -33,11 +33,11 @@ export const GET: APIRoute = async ({ params, request, site }) => {
       link: `/posts/${slug}`,
 
       content: sanitizeHtml(parser.render(body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
       }),
 
       customData: `<media:content
-          type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+          type="image/${data.image.format === "jpg" ? "jpeg" : "png"}"
           width="${data.image.width}"
           height="${data.image.height}"
           medium="image"
